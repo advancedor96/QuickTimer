3a43092b273208109fba3fdfa8f8cdf535ec68dd
丁丁 <dingding@bxb.tw>
2019-12-19 18:01:21 +0800

    做到一半，可選檔案得uri，但在broadcast播放有問題

--- app/src/main/java/com/dingding/quicktimer/AlarmReceiver.java
+++ app/src/main/java/com/dingding/quicktimer/AlarmReceiver.java
@@ -1,23 +1,101 @@
 package com.dingding.quicktimer;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Vibrator;
+import android.util.Log;
 import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import java.io.IOException;
 
 public class AlarmReceiver extends BroadcastReceiver {
     Vibrator v;
+    public int notificationId = 0;
+    String CHANNEL_ID = "default_notification_channel_id";
+    MediaPlayer mp = null;
     @Override
     public void onReceive(Context context, Intent k2) {
         // TODO Auto-generated method stub
-        Toast.makeText(context, "收到", Toast.LENGTH_LONG).show();
+        Toast.makeText(context, "鬧鐘到！", Toast.LENGTH_LONG).show();
 
         v=(Vibrator)context.getSystemService(Context.VIBRATOR_SERVICE);
+
+        Log.d("xd123", "onReceive: 播放uri:" + MainActivity.uri);
+            mp = MediaPlayer.create(context, MainActivity.uri);
+//            mp.setDataSource(context, MainActivity.uri);
+            Log.d("xd123", "onReceive: 播放uri:" + MainActivity.uri);
+            mp.start();
+            Log.d("xd123", "onReceive: 執行stare");
+
+
         v.vibrate(3000);
+
+
+//        sendNotification("時間到", "給我停", context);
+
+    }
+
+    private void sendNotification(String messageTitle, String messageBody, Context context){
+
+
+//        Intent stopInt = new Intent(this, StopMusic.class);
+//        stopInt.setAction("stopMusic");
+//        PendingIntent directStop = PendingIntent.getBroadcast(context, 0, stopInt, 0);
+
+//        原本用這個單純的 Broadcast 方式
+//        Intent stopInt = new Intent(this, StopMusic.class);
+//        stopInt.setAction("stopMusic");
+//        PendingIntent stopPendingInt = PendingIntent.getBroadcast(this, 0, stopInt, 0);
+
+        // 顯示 ToStopMusic後，按Back鍵，要回到MainActivity的方式
+        // https://www.jianshu.com/p/678e2322fd41
+        // 第三種方式 addNextIntentWithParentStack 為自己看官網文件查到的
+        Intent toStopIntent = new Intent(context, MainActivity.class);  // ToStopMusic.class
+        toStopIntent.putExtra("showStopButton", true);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context).addNextIntentWithParentStack(toStopIntent);
+        PendingIntent goToMainWithStopButton = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+//        NotificationCompat.Action stopAction = new NotificationCompat.Action.Builder(android.R.drawable.ic_media_pause, "關閉警報", directStop).build();
+
+        NotificationCompat.Builder notificationCompatBuilder = new NotificationCompat.Builder(context
+                , CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.alert_dark_frame) //.mipmap.ic_launcher_foreground
+                .setContentTitle(messageTitle)
+                .setContentText(messageBody)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//                .addAction(stopAction)
+                .setAutoCancel(true)                        // 按下通知後，移除此通知
+                .setContentIntent(goToMainWithStopButton)            // 按下通知後，打開intent
+                .setFullScreenIntent(goToMainWithStopButton, true); // 把它變成這個(426抄的)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "地震警報通知", NotificationManager.IMPORTANCE_HIGH);
+
+            channel.setDescription("地震快來時提醒");
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        notificationManager.notify(notificationId, notificationCompatBuilder.build());
+
+
+
 
     }
 }
--- app/src/main/java/com/dingding/quicktimer/MainActivity.java
+++ app/src/main/java/com/dingding/quicktimer/MainActivity.java
@@ -8,14 +8,19 @@ import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.sql.Time;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     TextView tv_time;
     TimePicker timePicker;
     final static private int SET_TIME = 0;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd HH:mm:ss");
+    public static Uri uri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,26 +51,39 @@ public class MainActivity extends AppCompatActivity {
             Bundle bundle = data.getExtras();
             int hr  = bundle.getInt("hr");
             int min = bundle.getInt("min");
-            tv_time.setText(String.format("鬧鐘%02d:%02d", hr, min));
 
             Calendar calNow = Calendar.getInstance();
             Calendar calSet = (Calendar)calNow.clone();
             calSet.set(Calendar.HOUR_OF_DAY, hr);
             calSet.set(Calendar.MINUTE, min);
+//             calSet.set(Calendar.SECOND, 0);
+
+            // 以下2行測試用
+            calSet.set(Calendar.MINUTE, min -1 );
+             calSet.set(Calendar.SECOND, calNow.get(Calendar.SECOND) + 7);
+
+
             if(calSet.compareTo(calNow) <=0){
                 Toast.makeText(this, "跨天", Toast.LENGTH_SHORT).show();
                 Log.d("xd123", "onActivityResult: 跨天");
                 calSet.add(Calendar.DATE, 1);
             }
+
+            tv_time.setText(String.format("鬧鐘%s", dateFormat.format(calSet.getTime())));
             setAlarm(calSet);
+        }else if (requestCode == 10 && resultCode == RESULT_OK) {
+            uri = data.getData();
+            Log.d("xd123", "onActivityResult: uri:" + uri);
+
         }
     }
     private void setAlarm(Calendar targetCal) {
+        Log.d("xd123", "setAlarm: "+ targetCal.getTime().toString());
         Intent intent = new Intent(getBaseContext(), AlarmReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast( this.getApplicationContext(), 1, intent, PendingIntent.FLAG_UPDATE_CURRENT|  Intent.FILL_IN_DATA);
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, targetCal.getTimeInMillis(), pendingIntent);
-        Toast.makeText(this, "設好鬧鐘.", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "設好鬧鐘:" + dateFormat.format(targetCal.getTime()), Toast.LENGTH_LONG).show();
 
     }
 
@@ -72,4 +92,10 @@ public class MainActivity extends AppCompatActivity {
 
         timePicker.setMinute(nowMin + 5);
     }
+
+    public void openFile(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 10);
+    }
+
 }
